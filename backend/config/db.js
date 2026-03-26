26c34c8b58fb8f9f7dc0bfd294dc5e55eeb3846e
Soumitra-Mandal19 <mandal.soumitra@outlook.com>
2026-03-26 06:19:40 +0530

    Added cert.pem in backend folder

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 const fs = require('fs');
-
+const path = require('path');
 
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    ssl: {
-         ca: Buffer.from(process.env.DB_CERT || '', 'utf-8')
-    }
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    ca: fs.readFileSync(path.join('/etc/secrets', 'cert.pem'))
+  }
 });
 
 
