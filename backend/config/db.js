3667b99192ea6de795a5c9425b02bebe4e3af55b
Soumitra-Mandal19 <mandal.soumitra@outlook.com>
2026-03-26 06:20:22 +0530

    Added sslOptions in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,29 +1,32 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 const fs = require('fs');
-const path = require('path');
 
+
+let sslOptions = undefined;
+
+// Use certificate file if it exists
+if (process.env.DB_CERT_PATH && fs.existsSync(process.env.DB_CERT_PATH)) {
+    sslOptions = {
+        ca: fs.readFileSync(process.env.DB_CERT_PATH)
+    };
+}
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: {
-    ca: fs.readFileSync(path.join('/etc/secrets', 'cert.pem'))
-  }
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: sslOptions
 });
 
-
 db.getConnection((err, connection) => {
-    if(err) {
+    if (err) {
         console.log("Error in connection with DB:", err);
-        
     } else {
         console.log("Connected To TibCloud DB!");
         connection.release();
-        
     }
 });
 
